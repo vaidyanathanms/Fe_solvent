dc955cd5fde529d724d77544d1b1c75475d05f0d
vaidyanathanms_cades <vm5@ornl.gov>
2026-03-05 22:24:49 -0500

    changed from main.trr to main_nojump.trr as inp

--- src_sh/run_ssip.sh
+++ src_sh/run_ssip.sh
@@ -5,19 +5,19 @@
 
 #SBATCH -A chem
 #SBATCH -p burst
-#SBATCH -t 10:00:00
+#SBATCH -t 05:00:00
 #SBATCH -N 1                    # 1 nodes
 #SBATCH --ntasks-per-node=1     # 1 tasks/node
 #SBATCH -c 1                    # 1 cores/task
 #SBATCH --mem=0
-#SBATCH -J ssip_Fe3_m_0.1
-#SBATCH -o ana_mol_0.1/out.%J
-#SBATCH -e ana_mol_0.1/err.%J
+#SBATCH -J ssip_Fe3_m_5.0
+#SBATCH -o ana_mol_5.0/out.%J
+#SBATCH -e ana_mol_5.0/err.%J
 
 # Load modules
 module reset
 module load intel mkl
-module load python/3.10.14
+module load python/3.15.04
 
 # Export gcc path and num_threads
 export LD_LIBRARY_PATH=/sw/cades-open/gcc/12.2.0/lib64:$LD_LIBRARY_PATH
@@ -28,7 +28,7 @@ cd $SLURM_SUBMIT_DIR
 echo "begin job to analyze SSIP  @start time: ${date}"
 echo $PWD
 
-outdir='ana_mol_0.1'
+outdir='ana_mol_5.0'
 mkdir -p ${outdir}
 wait
 
