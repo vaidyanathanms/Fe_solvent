9bd2024409251273ef4a6dd8dcfb34f043a96c32
vaidyanathanms_cades <vm5@ornl.gov>
2026-03-05 22:16:56 -0500

    updated with if loops so that no backups are created

--- src_sh/run_trjconv.sh
+++ src_sh/run_trjconv.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1     # 1 tasks/node
 #SBATCH -c 8                    # 8 cores/task
 #SBATCH --mem=0
-#SBATCH -J trjconv_Fe3_m_0.1
+#SBATCH -J trjconv_Fe3_m_0.4
 #SBATCH -o outdir/out.%J
 #SBATCH -e outdir/err.%J
 
@@ -30,26 +30,57 @@ echo $PWD
 # Run command
 gmx="${HOME}/gromacs-2024.5/install/bin/gmx_mpi "
 
-# Compute diffusivity
+# All required files
+fgro60='confout_all_60ps.gro'
+ftpr60='npt_main_60ps.tpr'
+fgro00='confout_all_ps.gro'
+ftrrnojumplong='traj_npt_main_nojump.trr'
+fgronojumplong='traj_npt_main_nojump.gro'
+fgronojumpshort='traj_npt_smallrun_nojump.gro'
 
-# write gro file at the msd start time - have to use the FULL system and not parts of it
-echo "Generating confout file at 60 ps"
-echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o confout_all_60ps.gro -pbc whole -b 60000 -e 60000
-wait
 
-# generate tpr file at the msd start time
-echo "Generating tpr file at 60 ps"
-srun ${gmx} grompp -f npt_main.mdp -c confout_all_60ps.gro -p topol.top -o npt_main_60ps.tpr
-wait
+if [ ! -f "$fgro60" ]; then
+	# write gro file at the msd start time - have to use the FULL system and not parts of it
+	echo "Generating confout file at 60 ps"
+	echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o confout_all_60ps.gro -pbc whole -b 60000 -e 60000
+	wait
+fi
 
-# write gro file at the t=0 have to use the FULL system and not parts of it
-echo "Generating confout file at t=0 in NPT cycle"
-echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o confout_all_ps.gro -pbc whole -b 0 -e 0
-wait
+if [ ! -f "$ftpr60" ]; then
+	# generate tpr file at the msd start time
+	echo "Generating tpr file at 60 ps"
+	srun ${gmx} grompp -f npt_main.mdp -c confout_all_60ps.gro -p topol.top -o npt_main_60ps.tpr
+	wait
+fi
 
-# convert npt_main.trr with no jump for the region of interest - here use the first frame and not the frame at 60 ps
-echo "Convert to pbc nojump for the regions of interest"
-echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o traj_npt_main_nojump.trr -pbc nojump -b 50000 -e 99000 
-wait
+if [ ! -f "$fgro00" ]; then
+	# write gro file at the t=0 have to use the FULL system and not parts of it
+	echo "Generating confout file at t=0 in NPT cycle"
+	echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o confout_all_ps.gro -pbc whole -b 0 -e 0
+	wait	
+fi
+
+
+if [ ! -f "$ftrrnojumplong" ]; then
+	# convert npt_main.trr with no jump for the region of interest - here use the first frame and not the frame at 60 ps
+	echo "Convert to pbc nojump for the regions of interest"
+	echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o traj_npt_main_nojump.trr -pbc nojump -b 50000 -e 99000 
+	wait
+fi
+
+if [ ! -f "$fgronojumplong" ]; then
+	# convert traj_npt_main.trr to gro file with nojump
+	echo "Convert to pbc nojump in gro format for the regions of interesst"
+	echo "0" | srun ${gmx} trjconv -f traj_npt_main.trr -s npt_main.tpr -o traj_npt_main_nojump.gro -pbc nojump -b 50000 -e 99000 
+	wait
+fi
+
+if [ ! -f "$ftrrnojumpshort" ]; then
+	# convert npt_main_smallrun.trr with no jump for the region of interest - here use the first frame and not the frame at 60 ps
+	echo "Convert to pbc nojump for the small_md"
+	echo "0" | srun ${gmx} trjconv -f traj_npt_smallrun.trr -s npt_smallrun.tpr -o traj_npt_smallrun_nojump.gro -pbc nojump
+	wait
+fi
+
 
 echo "End of run.."
