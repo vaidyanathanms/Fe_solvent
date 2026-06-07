7b326d1dd64f5880682016ead9309e22581b223d
vms_laptop <vm5@ornl.gov>
2026-06-06 21:31:52 -0400

    generic octave/matlab code for couting commentlines from an output GROMACS xvg file

---
+++ src_mat/count_commentlines_xvg.m
@@ -0,0 +1,14 @@
+function nxvg_comments = count_commentlines_xvg(filename)
+  nxvg_comments = 0;
+  fxvg = fopen(filename);
+
+  while ~feof(fxvg)
+    line = fgetl(fxvg);
+     if ischar(line) && !startsWith(line, "#") && !startsWith(line, "@")
+       break
+     else
+       nxvg_comments++;
+     end
+   end
+   fclose(fxvg);
+
