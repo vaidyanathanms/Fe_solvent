e5fd740ef7f140ffbc4730c52ca6802867bcd1ce
vaidyanathanms_cades <vm5@ornl.gov>
2026-06-17 13:43:56 -0400

    updated codes

--- src_py/fit_tau.py
+++ src_py/fit_tau.py
@@ -2,14 +2,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 import glob
 import warnings
 from pathlib import Path
 import aux_plt as aux
 from scipy.optimize import curve_fit
+import math
+
+system = 'cades' # cades or lap
 
 # Color/line data; figure defaults
+sns.set_palette("colorblind")
 orange = '#FFA500'; dark_g = '#006400'; brown = '#8B4513'
 clr_arr = [dark_g,orange,brown,'b','k','m']
 mrk_arr = ['o','d','s']
@@ -24,15 +29,24 @@ plt.rcParams['font.serif'] = ['Arial'] + plt.rcParams['font.serif']
 fecharge = 3
 trialnum = 5
 molality = ['0.1','0.2','0.4','1.0','2.5','5.0']
+runtype  = ['smallrun','main']
+iontype  = ['ion','countion']
 
-# Directory details
-dirkey     = 'tau'
-dirsuffix  = 'results_all_trial'+str(trialnum)+'/fetfsi_'+str(fecharge)
-headdir    = '../../FeTFSI/' + dirsuffix
-resultdir  = headdir + '/' + dirkey + '_all'
-figdir     = '../../FeTFSI/figures/' + dirsuffix +'/' + dirkey + '_all'
-outheaddir = '../../FeTFSI/analyzed_results/results_all_trial' + \
-    str(trialnum) + 'fetfsi_' + str(fecharge) + '/tau_results'
+# Directory/file details
+if system == 'cades':
+    dirsuffix  = '/lustre/or-scratch/cades-birthright/vm5/fetfsi/fetfsi_3/trial_5'
+    resultdir  =  dirsuffix + '/results_all'
+    figdir     =  dirsuffix + '/results_all/figtau_all'
+    outheaddir =  dirsuffix + '/results_all/tau_results'
+else:
+    # Directory details
+    dirkey     = 'tau'
+    dirsuffix  = 'results_all_trial'+str(trialnum)+'/fetfsi_'+str(fecharge)
+    headdir    = '../../FeTFSI/' + dirsuffix
+    resultdir  = headdir + '/' + dirkey + '_all'
+    figdir     = '../../FeTFSI/figures/' + dirsuffix +'/' + dirkey + '_all'
+    outheaddir = '../../FeTFSI/analyzed_results/results_all_trial' + \
+        str(trialnum) + 'fetfsi_' + str(fecharge) + '/tau_results'
 
 
 if not os.path.isdir(figdir):
@@ -41,121 +55,136 @@ if not os.path.isdir(figdir):
 if not os.path.isdir(outheaddir):
     os.mkdir(outheaddir)
 
+if not os.path.isdir(resultdir):
+    raise RuntimeError(f'{resultdir} not found')    
+
 # Load data
 for inum,molval in enumerate(molality): # loop in runarr
 
-    outdir   = '../../FeTFSI/analyzed_results/mol_' + str(molval)
+    if not system == 'cades':
+        outdir   = '../../FeTFSI/analyzed_results/mol_' + str(molval)
+    else:
+        outdir   = outheaddir
+        
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
-        
+
     print(f"Analyzing {molval}")
     
-    anadir = resultdir + '/mol_' + molval
+    anadir = resultdir + '/ana_mol_' + molval
 
     if not os.path.isdir(anadir):
         warnings.warn(f'{anadir} does not exist')
         continue
+
+    for rtype in runtype:
+
+        print(f"Analyzing {rtype}")
+        
+        for atype in iontype:
+
+            print(f"Analyzing {atype}")
             
-    # Check file
-    list_fnames = glob.glob(anadir + '/Fsktion*small*')
-    if list_fnames == []:
-        warnings.warn(f'No Fskt small run files exist in {anadir}')
-        continue
+            # Check file
+            list_fnames = glob.glob(f'{anadir}/Fskt{atype}*{rtype}*')
+            if list_fnames == []:
+                warnings.warn(f'No Fskt{atype}*{rtype}* files exist in {anadir}')
+                continue
 
-    # Initialize arrays
-    q_arr    = np.array([])
-    tau_arr  = np.array([]); tauavg_arr = np.array([])
-    beta_arr = np.array([]); c0_arr = np.array([])
-    tauerr_arr  = np.array([])
-    betaerr_arr = np.array([]); c0err_arr = np.array([])
+            # Initialize arrays
+            q_arr    = np.array([])
+            tau_arr  = np.array([]); tauavg_arr = np.array([])
+            beta_arr = np.array([]); c0_arr = np.array([])
+            tauerr_arr  = np.array([])
+            betaerr_arr = np.array([]); c0err_arr = np.array([])
 
-    # Set output file and output figure data
-    fig1,ax1 = plt.subplots()
-    ax1.set_xlabel(r'$q$ (nm$^{-1}$)')
-    ax1.set_ylabel(r'$F$_s$(q,t)$')
-    plt.style.use('seaborn-colorblind')
-    plt.tight_layout()
+            # Set output file and output figure data
+            fig1,ax1 = plt.subplots()
+            ax1.set_xlabel(r'$q$ (nm$^{-1}$)')
+            ax1.set_ylabel(r'$F$_s$(q,t)$')
+            #plt.style.use('seaborn-colorblind')
+            plt.tight_layout()
     
-    # Loop through the files
-    for fname in list_fnames:
-        # Get qval
-        qval = float(Path(fname).name.split('_')[1])
-        # Open and parse file
-        with open(fname) as fin:
-            lines = (line.lstrip() for line in fin \
-                     if not line.lstrip().startswith('#') and \
-                     not line.lstrip().startswith('@'))
-            data  = np.loadtxt(lines,skiprows=1)
-            tdata = data[:,0]
-            fskt  = data[:,1]
+            # Loop through the files
+            for fname in list_fnames:
+                # Get qval
+                qval = float(Path(fname).name.split('_')[1])
+                # Open and parse file
+                with open(fname) as fin:
+                    lines = (line.lstrip() for line in fin \
+                             if not line.lstrip().startswith('#') and \
+                             not line.lstrip().startswith('@'))
+                    data  = np.loadtxt(lines,skiprows=1)
+                    tdata = data[:,0]
+                    fskt  = data[:,1]
         
-        # Smooth to avoid repeated values
-        time_smooth, fskt_smooth = aux.compress_repeated_y(tdata,fskt,\
-                                                           tolerance=0.0,\
-                                                           method="middle")
+                # Smooth to avoid repeated values
+                time_smooth, fskt_smooth = aux.compress_repeated_y(tdata,fskt,\
+                                                                   tolerance=0.0,\
+                                                                   method="middle")
 
         
-        fskt_smooth /= fskt_smooth[0]
+                fskt_smooth /= fskt_smooth[0]
         
-        # Check if y-values are going higher
-        if len(fskt_smooth[fskt_smooth > 1.0]) != 0:
-            warnings.warn(f'Fskt values are not monotonically decreasing for {qval}')
-            q_arr    = np.append(q_arr,qval)
-            tau_arr  = np.append(tau_arr,np.nan)
-            beta_arr = np.append(beta_arr,np.nan)
-            c0_arr   = np.append(c0_arr,np.nan)
-            tauerr_arr  = np.append(tauerr_arr,np.nan)
-            betaerr_arr = np.append(betaerr_arr,np.nan)
-            c0err_arr   = np.append(c0err_arr,np.nan)
-            continue
+                # Truncate to the first negative value
+                time_data, fskt_data = aux.truncate(time_smooth,fskt_smooth)
 
-        # Truncate to the first negative value
-        time_data, fskt_data = aux.truncate(time_smooth,fskt_smooth)
+                # Check if y-values are going higher
+                if len(fskt_data[fskt_data > 1.0]) != 0:
+                    warnings.warn(f'Fskt values are not monotonically decreasing for {qval}')
+                    q_arr    = np.append(q_arr,qval)
+                    tau_arr  = np.append(tau_arr,np.nan)
+                    beta_arr = np.append(beta_arr,np.nan)
+                    c0_arr   = np.append(c0_arr,np.nan)
+                    tauerr_arr  = np.append(tauerr_arr,np.nan)
+                    betaerr_arr = np.append(betaerr_arr,np.nan)
+                    c0err_arr   = np.append(c0err_arr,np.nan)
+                    continue
 
-        # Fit initial guesses
-        c0 = fskt_smooth[-1]
-        tau0 = time_data[len(time_data)//5]
-        beta0 = 0.8
-        init_guess = [tau0,beta0,c0]
-        bounds = ([1e-10, 0.01, 0.0],
-                  [np.inf, 2.0, np.inf]) # same order as init_guess
+                # Fit initial guesses
+                c0 = fskt_data[-1]
+                tau0 = time_data[len(time_data)//5]
+                beta0 = 0.8
+                init_guess = [tau0,beta0,c0]
+                bounds = ([1e-10, 0.01, 0.0],
+                          [np.inf, 2.0, np.inf]) # same order as init_guess
 
-        # Fit
-        popt, pcov = aux.curve_fit(stretched_exp,time_data,fskt_data,\
-                                   p0=p0,bounds=bounds,maxfev=100000)
-        tau_fit, beta_fit, c0_fit = popt
-        tau_avg = tau_fit * gamma(1.0 + 1.0/beta_fit)
-        perr = np.sqrt(np.diag(pcov))
-
-        # Save to arrays
-        q_arr      = np.append(q_arr,qval)
-        tau_arr    = np.append(tau_arr,tau_fit)
-        beta_arr   = np.append(beta_arr,beta_fit)
-        c0_arr     = np.append(c0_arr,c0_fit)
-        tauavg_arr = np.append(c0_arr,tau_avg)
-        tauerr_arr  = np.append(tauerr_arr,perr[0])
-        betaerr_arr = np.append(betaerr_arr,perr[1])
-        c0err_arr   = np.append(c0err_arr,perr[0])
+                # Fit
+                popt, pcov = aux.curve_fit(aux.stretched_exp,time_data,fskt_data,\
+                                           p0=init_guess,bounds=bounds,maxfev=100000)
+                tau_fit, beta_fit, c0_fit = popt
+                tau_avg = tau_fit * math.gamma(1.0 + 1.0/beta_fit)
+                perr = np.sqrt(np.diag(pcov))
                 
-        # Plot data
-        ax1.plot(time_smooth,fskt_smooth,"o",markersize=2,\
-                 color=clr_arr[inum],label='_data')
-        tfit = np.linspace(time_smooth.min(),time_smooth.max(), 1000)
-        yfit = aux.stretched_exp(tfit, *popt)
-        ax1.plot(tfit,yfit,"-",linewidth=2,\
-                 color=clr_arr[inum],label=str(molval) + ' m')
+                # Save to arrays
+                q_arr      = np.append(q_arr,qval)
+                tau_arr    = np.append(tau_arr,tau_fit)
+                beta_arr   = np.append(beta_arr,beta_fit)
+                c0_arr     = np.append(c0_arr,c0_fit)
+                tauavg_arr = np.append(c0_arr,tau_avg)
+                tauerr_arr  = np.append(tauerr_arr,perr[0])
+                betaerr_arr = np.append(betaerr_arr,perr[1])
+                c0err_arr   = np.append(c0err_arr,perr[0])
+                
+                # Plot data
+                ax1.plot(time_data,fskt_data,"o",markersize=2,\
+                         color=clr_arr[inum],label='_data')
+                tfit = np.linspace(time_data.min(),time_data.max(), 1000)
+                yfit = aux.stretched_exp(tfit, *popt)
+                ax1.plot(tfit,yfit,"-",linewidth=2,\
+                         color=clr_arr[inum],label=str(molval) + ' m')
 
         
-    # Save output file
-    combined_data = np.column_stack((q_arr,tau_arr,beta_arr,c0_arr,\
-                                     tauerr_arr,betaerr_arr,c0err_arr))
+                # Save output file
+                combined_data = np.column_stack((q_arr,tau_arr,beta_arr,c0_arr,\
+                                                 tauerr_arr,betaerr_arr,c0err_arr))
     
-    np.savetxt(outdir + '/tausmall_mol_' + str(molval)+'.dat',\
-               combined_data,delimiter='\t',\
-               header='q\ttau\tbeta\tc0\ttau_avg\ttau_err\tbeta_err\tc0_err')
-
-    plt.legend(loc=0)
-    plt.tight_layout()
-    fig1.savefig(figdir + '/tausmall_mol_' + str(molval)+ '.png',\
-                 dpi=fig1.dpi)
+                np.savetxt(f'{outdir}/tau{atype}_{rtype}_mol_{molval}.dat',\
+                           combined_data,delimiter='\t',\
+                           header='q\ttau\tbeta\tc0\ttau_avg\ttau_err\tbeta_err\tc0_err')
+                
+            plt.legend(loc=0)
+            plt.tight_layout()
+            fig1.savefig(f'{figdir}/tau{atype}_{rtype}_mol_{molval}.png',\
+                         dpi=fig1.dpi)
 
--- src_py/plt_diff_all.py
+++ src_py/plt_diff_all.py
@@ -6,34 +6,40 @@ import pandas as pd
 import os
 import warnings
 import aux_plt as aux
+import seaborn as sns
+
+system   = 'cades' #cades or lap
 
 # Color/line data; figure defaults
+sns.set_palette("colorblind")
 orange = '#FFA500'; dark_g = '#006400'; brown = '#8B4513'
 clr_arr = [dark_g,orange,brown,'b','k','m']
 mrk_arr = ['o','d','s']
 lne_arr = ['-','--']
+if system=='cades':
+    font_dir = os.path.expanduser('~/.fonts')
 plt.rc('legend',fontsize=16) # fontsize of the legends
 plt.rcParams.update({'font.size': 14}) # other fonts
 plt.rcParams.update({'figure.autolayout': True})
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['font.serif'] = ['Arial'] + plt.rcParams['font.serif']
+plt.rcParams['font.sans-serif'] = ['Liberation Sans', 'DejaVu Sans', 'Arial']
+plt.rcParams['font.family'] = 'sans-serif'
 
 # Inputs
 fecharge = 3
 trialnum = 5
 dirkey   = 'diffusivity'
-system   = 'cades' #cades or lap
+
 # Directory/file details
 if system == 'cades':
     dirsuffix  = '/lustre/or-scratch/cades-birthright/vm5/fetfsi/fetfsi_3/trial_5/results_all'
-    anadir     =  dirsuffix + '/density_all'
+    anadir     =  dirsuffix + '/dens_diff_all'
     figdir     =  dirsuffix + '/figs_all'
 else:
     dirsuffix  = 'results_all_trial'+str(trialnum)
     anadir     = '../../FeTFSI/analyzed_results/' +  dirsuffix + '/density_all'
     figdir     = '../../FeTFSI/figures/' + dirsuffix +'/fetfsi_'+str(fecharge) +'/' + dirkey + '_all'
     
-inpfile = f'{anadir}/density.xlsx'
+inpfile = f'{anadir}/dens_and_diff.xlsx'
 
 if not os.path.isdir(anadir):
     raise RuntimeError(f'analysis directory: {anadir} not found')
@@ -51,8 +57,8 @@ print(df.columns)
 x = df["mol"]
 y_fe = df["d_Fe(cm2/s)"]
 err_fe = df["ed_Fe(cm2/s)"]
-y_f = df["d_F(cm2/s)"]
-err_f = df["ed_F(cm2/s)"]
+y_tfsi = df["d_TFSI_COM(cm^2/s)"]
+err_tfsi = df["ed_TFSI_COM(cm^2/s)"]
 
 if not os.path.isdir(figdir):
     os.mkdir(figdir)
@@ -60,39 +66,37 @@ if not os.path.isdir(figdir):
 #-------------------------------------------------------------------------------
 # Create scatter plot with error bars
 fig1,ax1 = plt.subplots()
-ax1.set_xlabel(r'Concentration (m)')
-ax1.set_ylabel(r'Diffusivity (m$^2$/s)')
-if not system == 'cades':
-    plt.style.use('seaborn-colorblind')
+ax1.set_xlabel(r'Concentration (m)',fontsize=16)
+ax1.set_ylabel(r'Diffusivity (m$^2$/s)',fontsize=16)
+plt.style.use('seaborn-colorblind')
 plt.tight_layout()
 print("Plotting Fe/TFSI diffusivities ...")
 plt.errorbar(x, 0.0001*y_fe, yerr=0.0001*err_fe,color=clr_arr[0],\
              marker='o',markersize=8,capsize=4, linestyle='none',label=r'Fe$^{3+}$')
 
-plt.errorbar(x, 0.0001*y_f, yerr=0.0001*err_f,color=clr_arr[1],\
+plt.errorbar(x, 0.0001*y_tfsi, yerr=0.0001*err_tfsi,color=clr_arr[1],\
              marker='s',markersize=8,capsize=4, linestyle='none',label=r'TFSI$^{-}$')
 
-plt.legend()
+ax1.legend()
 fig1.savefig(figdir + '/diff_all_Fe' + str(fecharge) +
              '_trial_' + str(trialnum) + '.png',dpi=fig1.dpi)
 #-------------------------------------------------------------------------------
 
 # Create scatter plot with error bars in semilog scale
 fig2,ax2 = plt.subplots()
-ax2.set_xlabel(r'Concentration (m)')
-ax2.set_ylabel(r'Diffusivity (m$^2$/s)')
-if not system == 'cades':
-    plt.style.use('seaborn-colorblind')
+ax2.set_xlabel(r'Concentration (m)',fontsize=16)
+ax2.set_ylabel(r'Diffusivity (m$^2$/s)',fontsize=16)
+plt.style.use('seaborn-colorblind')
 plt.tight_layout()
 print("Plotting Fe/TFSI diffusivities in semilog scale...")
 plt.errorbar(x, 0.0001*y_fe, yerr=0.0001*err_fe,marker='o',\
              capsize=4, linestyle='none',label=r'Fe$^{3+}$')
 
-plt.errorbar(x, 0.0001*y_f, yerr=0.0001*err_f,marker='s',\
+plt.errorbar(x, 0.0001*y_tfsi, yerr=0.0001*err_tfsi,marker='s',\
              capsize=4, linestyle='none',label=r'TFSI$^{-}$')
 
 plt.yscale('log')
-plt.legend()
+ax2.legend()
 fig2.savefig(figdir + '/logdiff_all_Fe' + str(fecharge) +
              '_trial_' + str(trialnum) + '.png',dpi=fig1.dpi)
 
@@ -103,32 +107,32 @@ fig2.savefig(figdir + '/logdiff_all_Fe' + str(fecharge) +
 # Keep only positive diffusivities
 print("Computing VFT fits ...")
 mask_fe = (y_fe > 0)
-mask_f  = (y_f > 0)
+mask_tfsi  = (y_tfsi > 0)
 
 x_fe = x[mask_fe]
-x_f  = x[mask_f]
+x_tfsi  = x[mask_tfsi]
 
 y_fe = y_fe[mask_fe]
 err_fe = err_fe[mask_fe]
 
-y_f = y_f[mask_f]
-err_f = err_f[mask_f]
+y_tfsi = y_tfsi[mask_tfsi]
+err_tfsi = err_tfsi[mask_tfsi]
 
 # natural log of diffusivity
-logy_fe = np.log(y_fe)
-logy_f  = np.log(y_f)
+logy_fe    = np.log(y_fe)
+logy_tfsi  = np.log(y_tfsi)
 
 # Propagated uncertainty in log(D): sigma_logD ~ sigma_D / D
 # Put a fallback if any error bar is zero or missing
 eps = 1e-30
 siglog_fe = np.where(err_fe > 0, err_fe / np.maximum(y_fe, eps), 1.0)
-siglog_f  = np.where(err_f  > 0, err_f  / np.maximum(y_f,  eps), 1.0)
+siglog_f  = np.where(err_tfsi  > 0, err_tfsi  / np.maximum(y_tfsi,  eps), 1.0)
 
 xmax = np.max(x)
 
 # Initial guesses
 p0_fe = [np.log(np.max(y_fe)), 1.0, xmax + 1.0]
-p0_f  = [np.log(np.max(y_f)),  1.0, xmax + 1.0]
+p0_f  = [np.log(np.max(y_tfsi)),  1.0, xmax + 1.0]
 
 # Bounds: c0 must be > max(x)
 lower_bounds = [-np.inf, 0.0, xmax + 1e-6]
@@ -146,14 +150,14 @@ perr_fe = np.sqrt(np.diag(pcov_fe))
 logD0_fe_err, B_fe_err, c0_fe_err = perr_fe
 
 # Fit F in log-space
-popt_f, pcov_f = aux.curve_fit(aux.log_vft_conc,x_f,logy_f,p0=p0_f,\
+popt_f, pcov_f = aux.curve_fit(aux.log_vft_conc,x_tfsi,logy_tfsi,p0=p0_f,\
                                sigma=siglog_f,absolute_sigma=True,\
                                bounds=(lower_bounds, upper_bounds),
                                maxfev=20000)
 
 logD0_f, B_f, c0_f = popt_f
-perr_f = np.sqrt(np.diag(pcov_f))
-logD0_f_err, B_f_err, c0_f_err = perr_f
+perr_tfsi = np.sqrt(np.diag(pcov_f))
+logD0_f_err, B_f_err, c0_f_err = perr_tfsi
 
 # Convert logD0 back to D0
 D0_fe = np.exp(logD0_fe)
@@ -174,29 +178,26 @@ yfit_f  = np.exp(logfit_f)
 
 # Plot
 fig3,ax3 = plt.subplots()
-ax1.set_xlabel(r'Concentration (m)')
-ax1.set_ylabel(r'Diffusivity (m$^2$/s)')
-if not system == 'cades':
-    plt.style.use('seaborn-colorblind')
+plt.style.use('seaborn-colorblind')
 plt.tight_layout()
 print("Plotting Fe/TFSI diffusivities with VFT fits...")
-plt.errorbar(x, 0.0001*y_fe, yerr=0.0001*err_fe,marker='o',\
+plt.errorbar(x, y_fe, yerr=err_fe,marker='o',\
              color=clr_arr[0],markersize=8,capsize=4, \
              linestyle='none',label=r'Fe$^{3+}$')
 
-plt.errorbar(x, 0.0001*y_f, yerr=0.0001*err_f,marker='s',\
+plt.errorbar(x, y_tfsi, yerr=err_tfsi,marker='s',\
              color = clr_arr[1],markersize=8,capsize=4, \
              linestyle='none',label='TFSI$^{-}$')
 
 plt.plot(xfit, yfit_fe,color=clr_arr[0],linestyle='--',\
-         linewidth = 2.5, label=r'VFT Fit - Fe$^{3+}$)')
+         linewidth = 2.5, label=r'VFT Fit - Fe$^{3+}$')
 plt.plot(xfit, yfit_f,color=clr_arr[1],linestyle='--',\
          linewidth = 2.5, label=r'VFT Fit - TFSI$^{-}$')
 
 plt.yscale("log")
-plt.xlabel("Concentration (m)")
-plt.ylabel("Diffusivity (m$^2$/s)")
-plt.legend()
+ax3.set_xlabel("Concentration (m)",fontsize=16)
+ax3.set_ylabel("Diffusivity (cm$^2$/s)",fontsize=16)
+ax3.legend()
 plt.tight_layout()
 fig3.savefig(figdir + '/VFTfit_diff_all_Fe' + str(fecharge) +
              '_trial_' + str(trialnum) + '.png',dpi=fig3.dpi)
@@ -219,11 +220,10 @@ with open(f'{anadir}/D_fitted.dat','w') as fid:
 
 # Plot D_Fe/D_F
 fig4,ax4 = plt.subplots()
-rat_data = df['d_F/d_Fe']
-ax1.set_xlabel(r'Concentration (m)')
-ax1.set_ylabel(r'$D_\rm{TFSI^{-}}$/$D_\rm{Fe^{3+}}$')
-if not system == 'cades':
-    plt.style.use('seaborn-colorblind')
+rat_data = df['d_TFSICOM/d_Fe']
+ax4.set_xlabel(r'Concentration (m)',fontsize=16)
+ax4.set_ylabel(r'$D_{\rm{TFSI}^{-}}$/$D_{\rm{Fe}^{3+}}$',fontsize=16)
+plt.style.use('seaborn-colorblind')
 plt.tight_layout()
 print("Plotting ratio of Fe/TFSI diffusivities")
 plt.plot(x, rat_data, marker='o',color=clr_arr[0],\
